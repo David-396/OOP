7c70d5d36f18634b04ca7731750b3a1c383e8169
David-396 <david.eini12@gmail.com>
2025-06-25 09:40:06 +0300

    fe

--- Square.py
+++ Square.py
@@ -1,11 +1,10 @@
 from Shapes import Shape
 
 class Square(Shape):
-    def __init__(self, height, length):
+    def __init__(self, side):
         super().__init__()
-        self.height = height
-        self.length = length
+        self.side =side
 
     def get_area(self):
-        return self.height*self.length
+        return self.side**2
 
